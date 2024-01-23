93540d92d1997770ada0ce7f874c95fd2af597e4
MyNgoc1 <145568770+MyNgoc1@users.noreply.github.com>
2024-01-23 08:45:21 +0700

    update_hello

---
+++ bai1_hello/src/bai1_hello/SayHelloApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package bai1_hello;
+
+/**
+ * 
+ */
+public class SayHelloApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("xin chao. toi la sinh vien khoa 63 CNTT. Hoc lap trinh Java");
+		System.out.print("Mai Cuong Tho");
+	}
+
+}
